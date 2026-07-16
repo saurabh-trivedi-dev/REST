03e33e186dc6b64be1fd32f954c54c4418875171
saurabh-trivedi-dev <saurabhtrivedi.dev@gmail.com>
2026-07-17 02:49:16 +0530

    Fastapi status codes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi import status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -23,7 +24,17 @@ students = []
 next_id = 1
 
 
-@app.post("/students", response_model=Student)
+def find_student_index(student_id: int):
+    for index, student in enumerate(students):
+        if(student.id == student_id):
+            return index
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Student not found"
+    )
+
+
+@app.post("/students", response_model=Student, status_code=status.HTTP_201_CREATED)
 def create_student(student: StudentCreate):
     global next_id
     new_student = Student(
@@ -35,15 +46,38 @@ def create_student(student: StudentCreate):
     )
     next_id+=1
     students.append(new_student)
-    return {
-        "message": "Student Created Successfully!",
-        "student": new_student
-    }
+    return new_student
 
 
-@app.get("/students", response_model=list[Student])
+@app.get("/students", response_model=list[Student], status_code=status.HTTP_200_OK)
 def get_students():
     return students
+
+
+@app.get("/students/{student_id}", response_model=Student, status_code=status.HTTP_200_OK)
+def get_student(student_id: int):
+    index = find_student_index(student_id)
+    return students[index]
+        
+
+@app.put("/students/{student_id}", response_model=Student, status_code=status.HTTP_200_OK)
+def update_student(student_id: int, updated_student:StudentCreate):
+    index = find_student_index(student_id)
+    students[index].name = updated_student.name
+    students[index].age = updated_student.age
+    students[index].course = updated_student.course
+    students[index].cgpa = updated_student.cgpa
+    return students[index]
+
+
+@app.delete("/students/{student_id}", status_code=status.HTTP_200_OK)
+def delete_student(student_id: int):
+    index = find_student_index(student_id)
+    del students[index]
+    return{
+        "message" : "Student Deleted Successfully!"
+    }
+
 
 
 @app.get("/students/filter", response_model=list[Student])
@@ -58,37 +92,6 @@ def filter_students(course: str, cgpa: float):
     return filtered_students
 
 
-@app.get("/students/{name}", response_model=Student)
-def get_student(name: str):
-    for student in students:
-        if student.name.lower() == name.lower():
-            return student
-    
-    raise HTTPException(status_code=404, detail="Student not found")
-
-
-# @app.put("/students/{name}")
-# def update_student(name: str, updated_student:Student):
-#     for index, student in enumerate(students):
-#         if(student.name.lower() == name.lower()):
-#             updated_student.name = student.name
-#             students[index] = updated_student
-#             return {
-#                 "message" : "Student Found!!",
-#                 "student" : updated_student
-#             }
-#     raise HTTPException(status_code=404, detail="Student is not in the records!")
-
-
-# @app.delete("/students/{name}")
-# def delete_student(name: str):
-#     for index, student in enumerate(students):
-#         if student.name.lower() == name.lower():
-#             del students[index]
-#             return {
-#                 "message" : "Student Deleted Successfully!"
-#             }
-#     raise HTTPException(status_code=404, detail="Student is not in the records!")
 
 
 # # python3 -m uvicorn main:app --reload (to reload the site everytime something is changed)
